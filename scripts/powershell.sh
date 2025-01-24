2a8735bb86528bd18fdb355853df1a43a205b833
ryzen <111833605+ryzenpay@users.noreply.github.com>
2025-01-24 14:52:27 -0500

    Create powershell.sh

---
+++ scripts/powershell.sh
@@ -0,0 +1,25 @@
+#https://learn.microsoft.com/en-us/powershell/scripting/install/install-debian?view=powershell-7.5#installation-on-debian-11-or-12-via-the-package-repository
+###################################
+# Prerequisites
+
+# Update the list of packages
+sudo apt-get update
+
+# Install pre-requisite packages.
+sudo apt-get install -y wget
+
+# Download the PowerShell package file
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.4.7/powershell_7.4.7-1.deb_amd64.deb
+
+###################################
+# Install the PowerShell package
+sudo dpkg -i powershell_7.4.7-1.deb_amd64.deb
+
+# Resolve missing dependencies and finish the install (if necessary)
+sudo apt-get install -f
+
+# Delete the downloaded package file
+rm powershell_7.4.7-1.deb_amd64.deb
+
+# Start PowerShell
+pwsh
